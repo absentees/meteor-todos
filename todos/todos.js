690577fe9808caebe801bf0fca519dd3457b3987
absentees <scottblissett@gmail.com>
2015-08-24 21:08:53 +1000

    pub and sub

--- todos/todos.js
+++ todos/todos.js
@@ -195,43 +195,35 @@ if (Meteor.isClient) {
     'submit form': function(event) {
       event.preventDefault();
       /*
-      var email = $('[name=email]').val();
-      var password = $('[name=password]').val();
-      Accounts.createUser({
-        email: email,
-        password: password
-      }, function(error){
-        if (error) {
-          console.log(error.reason);
-        }
-        else{
-          Router.go("home");
-        }
-      });
+
       */
     }
   });
 
   Template.register.onRendered(function() {
-    $('.register').validate();
+    $('.register').validate({
+      submitHandler: function(event) {
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Accounts.createUser({
+          email: email,
+          password: password
+        }, function(error) {
+          if (error) {
+            console.log(error.reason);
+          } else {
+            Router.go("home");
+          }
+        });
+      }
+    });
   });
 
   Template.login.events({
     'submit form': function(event) {
       event.preventDefault();
       /*
-      var email = $('[name=email]').val();
-      var password = $('[name=password]').val();
-      Meteor.loginWithPassword(email,password, function(err){
-        if (err) {
-          console.log(err.reason);
-        } else {
-          var currentRoute = Router.current().route.getName();
-          if (currentRoute == "login") {
-            Router.go("home");
-          }
-        }
-      });
+
       */
     }
   });
@@ -249,7 +241,31 @@ if (Meteor.isClient) {
   });
 
   Template.login.onRendered(function() {
-    $('.login').validate();
+    var validator = $('.login').validate({
+      submitHandler: function(event) {
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Meteor.loginWithPassword(email, password, function(err) {
+          if (err) {
+            if (err.reason == "User not found") {
+              validator.showErrors({
+                email: "That email doesn't belong to a registered user."
+              })
+            }
+            if (err.reason == "Incorrect password") {
+              validator.showErrors({
+                password: "You entered an incorrect password."
+              });
+            }
+          } else {
+            var currentRoute = Router.current().route.getName();
+            if (currentRoute == "login") {
+              Router.go("home");
+            }
+          }
+        });
+      }
+    });
   });
 
   Template.login.onDestroyed(function() {
